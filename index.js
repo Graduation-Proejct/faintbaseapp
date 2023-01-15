4791c8ea727c83095283fcb7a2affca20ac92b97
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-01-15 22:12:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dbRoute = require("./routes/login_signup_route.js");
 
 const app = express();
 
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
